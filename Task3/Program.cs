69205653b8164cc4a025cd02b1619c4281579a59
Daria Silkina <kuteneva.daria@yandex.ru>
2024-03-10 15:11:07 +0300

    add 7lesson:task3

---
+++ Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+// Задайте произвольный массив. 
+// Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1
+
+
+void ElementEndStart(int[] Array, int n)
+{
+    if (n > 0)
+    {
+        Console.Write(Array[n] + " ");
+        ElementEndStart(Array, n - 1);
+    }
+    else Console.WriteLine(Array[0]);
+}
+
+{
+    int[] num = { 1, 2, 5, 0, 10, 34 };
+    ElementEndStart(num, num.Length - 1);
+}
+
+
+
